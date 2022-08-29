151aa8d04f8b1cb329f0b0941abdfaf814ebb2ad
Gabriellopes232 <xdynatz@gmail.com>
2022-08-29 16:45:28 -0300

    feat: structure curriculum page

--- src/components/pages/curriculum/index.tsx
+++ src/components/pages/curriculum/index.tsx
@@ -1,34 +1,34 @@
 import SkillsContainer from "../resume/skill";
+import { Competencies, CurriculumText } from "./styles";
 
 const Curriculum: React.FC = () => {
+    interface curriculumProps {
+        id: number
+        title: string
+        text: string
+    }
+    const curr: curriculumProps[] = [
+        {
+            id: 1,
+            title: 'Curriculum Vitae',
+            text: 'See here my skills, my curriculum and my knowledge'
+        },
+    ]
     return (
         <div>
-            <section>
-                <h3>Curriculum Vitae</h3>
-                <p>
-                    See here my skills, my curriculum and my knowledge
-                </p>
-            </section>
-            <div>
-                <h3>Index</h3>
-                <span>
-                    <a href="">Competencies</a>
-                </span>
-                <span>
-                    <a href="">Experience</a>
-                </span>
-                <span>
-                    <a href="">Education</a>
-                </span>
-                <span>
-                    <a href="">Download my CV</a>
-                </span>
-            </div>
-
-            <section>
-                <h3>#Competencies</h3>
+            {curr.map(arr => (
+                <CurriculumText key={arr.id}>
+                    <h3>{arr.title}</h3>
+                    <p>
+                        {arr.text}
+                    </p>
+                    <a href="./public/resume.pdf" download='resume.pdf'> - Download/See my resume as a PDF</a>
+                </CurriculumText>
+            ))}
+            <Competencies>
+                <h3>Competencies</h3>
                 <SkillsContainer />
-            </section>
+            </Competencies>
         </div>
     );
 };
