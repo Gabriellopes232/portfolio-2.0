caf5e51a7c24b8cdb93de8a1664bd891c3ba8aac
Gabriellopes232 <xdynatz@gmail.com>
2022-08-31 12:49:21 -0300

    fix: link resume pdf

--- src/components/mini-components/curriculum/index.tsx
+++ src/components/mini-components/curriculum/index.tsx
@@ -22,7 +22,7 @@ const Curriculum: React.FC = () => {
                     <p>
                         {arr.text}
                     </p>
-                    <a href="./public/resume.pdf" download='resume.pdf'> - Download/See my resume as a PDF</a>
+                    <a href="resume.pdf" download='resume.pdf'> - Download/See my resume as a PDF</a>
                 </CurriculumText>
             ))}
             <Competencies>
