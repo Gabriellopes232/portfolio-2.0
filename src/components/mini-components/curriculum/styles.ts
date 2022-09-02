d095886606a79e9a08df625892b603504c46831e
Gabriellopes232 <xdynatz@gmail.com>
2022-09-02 12:28:17 -0300

    feat: styles experience box

--- src/components/mini-components/curriculum/styles.ts
+++ src/components/mini-components/curriculum/styles.ts
@@ -4,7 +4,28 @@ export const CurriculumText = styled.section`
     display: grid;
     padding-bottom: 2em;
     gap: 3em;
+    section {
+      display: grid;
+      align-content:center;
+      text-align: center;
+      width: 45%;
+      height: 3em;
+      max-height: 4em;
+      margin: 0;
+      border-radius: 1em;
+      background-color:#88888808;
 
+      p {
+          display: inline-block;
+          font-size: 1em;
+          font-weight: 400;
+          line-height: 150%;
+         
+         .detail {
+            font-weight: 600;
+         }
+      }
+    }
     h3::after{
         display: block;
         content: "";
@@ -13,8 +34,8 @@ export const CurriculumText = styled.section`
         background-color: ${(props) => props.theme.colors.links};
         opacity: 0.5;
         font-style: normal;
-        margin-top: 0.8rem;
-        border-radius: 1rem;
+        margin-top: 0.8em;
+        border-radius: 1em;
     }
 `
 
@@ -31,7 +52,7 @@ export const Competencies = styled.section`
         background-color: ${(props) => props.theme.colors.boxshadow};
         opacity: 0.5;
         font-style: normal;
-        margin-top: 0.8rem;
-        border-radius: 1rem;
+        margin-top: 0.8em;
+        border-radius: 1em;
     }
 `
