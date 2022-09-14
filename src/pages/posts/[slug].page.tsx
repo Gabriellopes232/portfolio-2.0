41117fcccf0e94be45ce05701f9e71cb3ebf0d80
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-09-14 16:32:55 -0300

    feat: slugpage blog

---
+++ src/pages/posts/[slug].page.tsx
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import md from 'markdown-it';
+import path from 'path';
+
+type Props = {
+  params: {
+    slug: any
+  }
+}
+
+type Teste = {
+  frontmatter: any
+  content: any
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync('posts');
+    const paths = files.map((fileName) => ({
+        params: {
+          slug: fileName.replace('.md', ''),
+        },
+    }));
+    return {
+        paths,
+        fallback: false,
+    };
+}
+export async function getStaticProps({ params: { slug } }: Props) {
+    const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8');
+    const { data: frontmatter, content } = matter(fileName);
+    return {
+      props: {
+        frontmatter,
+        content,
+      },
+    };
+}
+export default function PostPage({ frontmatter, content }: Teste) {
+    return (
+      <div className='prose mx-auto'>
+        <p>{frontmatter.title}</p>
+        <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+      </div>
+    );
+  }
