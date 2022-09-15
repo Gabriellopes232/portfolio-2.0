28db35367697f71bb4b45a72e6d71665af5e076a
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-09-15 15:10:56 -0300

    feat: next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+const seoConfig = {
+    title: 'Portfolio site',
+    description: 'Website with some information about me and my work',
+    openGraph: {
+        type: 'website',
+        locale: 'pt_BR',
+        url: 'https://portfolio-2-0-gamma.vercel.app/',
+        site_name: 'SiteName',
+    },
+    twitter: {
+        handle: '@gabriellopesxz',
+        site: '@site',
+        cardType: 'summary_large_image',
+    },
+};
+export default seoConfig;
