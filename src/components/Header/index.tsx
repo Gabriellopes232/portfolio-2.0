7c6417fb45663e71e30c334ce5e563d511434b84
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2022-09-22 15:29:12 -0300

    feat: add accessibility

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useTheme } from '../../styles/themes/ThemeContext';
 import { TablerBrandTwitter, FeInstagram, MdiGithub, DarkModeOutlineRounded, LightModeOutline } from '../../styles/icons';
-import { Container } from './styles';
+import { Container, IconButton } from './styles';
 
 const Header: React.FC = () => {
     const themeToggle = useTheme();
@@ -11,14 +11,17 @@ const Header: React.FC = () => {
             <Link href='/'>Profile</Link>
             <Link href='/blog'>Blog</Link>
             <Link href='/projects'>Projects</Link>
-            <a href="https://twitter.com/Gabriellopesxz" target="_blank" rel="noreferrer"><TablerBrandTwitter /></a>
-            <a href="https://www.instagram.com/gabriellopesxz/" target="_blank" rel="noreferrer"><FeInstagram /></a>
-            <a href="https://github.com/Gabriellopes232" target="_blank" rel="noreferrer"><MdiGithub /></a>
-            <a
+            <a href="https://twitter.com/Gabriellopesxz" target="_blank" rel="noreferrer" aria-label="link twitter"><TablerBrandTwitter /></a>
+            <a href="https://www.instagram.com/gabriellopesxz/" target="_blank" rel="noreferrer" aria-label='link instagram'><FeInstagram /></a>
+            <a href="https://github.com/Gabriellopes232" target="_blank" rel="noreferrer" aria-label='link github'><MdiGithub /></a>
+            <IconButton
+                type='button'
+                aria-label='button theme'
                 onClick={() => themeToggle?.toggleTheme()}
             >
                 {themeToggle?.currentTheme === 'light' ? <DarkModeOutlineRounded /> : <LightModeOutline />}
-            </a>
+            </IconButton>
+
         </Container>
     );
 };
