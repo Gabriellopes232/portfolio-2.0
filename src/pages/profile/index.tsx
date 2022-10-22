713b5416f6ce4f456028a714448a93ee8a883c39
Gabriel Lopes <gabriel.lopes.nascimento.dev@gmail.com>
2022-10-22 15:10:24 -0300

    fix: infos

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -9,7 +9,7 @@ const Profile = () => {
                 Hello my name is Gabriel and Im 19 years old Im a Software Engineer Jr. passionate about technology.
             </p>
             <p>
-                Today I work for a year in the area of web development, passionate about react and its features, I seek more and more to evolve and seek more and more perfection.
+                Today I work for two years in the area of web development, passionate about react and its features, I seek more and more to evolve and seek more and more perfection.
             </p>
             <p>
                 I love taking photos and soon I will be evolving more and more in this hobby until then you can find me on <a href="https://www.instagram.com/gabriellopesxz/" target="_blank" rel="noreferrer">Instagram</a> which will be where I will deposit my photos.
